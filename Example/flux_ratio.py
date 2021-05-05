1f8b5a411e3e11c77d1e76188f8622f461f1f224
skterry <sterry4261@gmail.com>
2021-05-04 22:55:13 -0700

    update flux_ratio calc

--- Example/flux_ratio.py
+++ Example/flux_ratio.py
@@ -11,23 +11,45 @@ if starNum == 3:
 	s2 = float(input("Star 2 Mag : "))
 	s3 = float(input("Star 3 Mag : "))
 
-	s1_fratio = (10**(-0.4*s1))/((10**(-0.4*s1))+(10**(-0.4*s2))+(10**(-0.4*s3)))
-	s2_fratio = (10**(-0.4*s2))/((10**(-0.4*s1))+(10**(-0.4*s2))+(10**(-0.4*s3)))
-	s3_fratio = (10**(-0.4*s3))/((10**(-0.4*s1))+(10**(-0.4*s2))+(10**(-0.4*s3)))
+	s1_s2_fratio = (10**(-0.4*s1))/(((10**(-0.4*s2))))
+	s1_s3_fratio = (10**(-0.4*s1))/(((10**(-0.4*s3))))
 
-	print("Star 1 ratio:", s1_fratio)
-	print("Star 2 ratio:", s2_fratio)
-	print("Star 3 ratio:", s3_fratio)
+	s2_s1_fratio = (10**(-0.4*s2))/(((10**(-0.4*s1))))
+	s2_s3_fratio = (10**(-0.4*s2))/(((10**(-0.4*s3))))
+
+	s3_s1_fratio = (10**(-0.4*s3))/(((10**(-0.4*s1))))
+	s3_s2_fratio = (10**(-0.4*s3))/(((10**(-0.4*s2))))
+	print("-----------------------------------")
+	print("Star 1/Star2 ratio:", s1_s2_fratio)
+	print("Star 1/Star3 ratio:", s1_s3_fratio)
+	print("Star 2/Star1 ratio:", s2_s1_fratio)
+	print("Star 2/Star3 ratio:", s2_s3_fratio)
+	print("Star 3/Star1 ratio:", s3_s1_fratio)
+	print("Star 3/Star2 ratio:", s3_s2_fratio)
+
+	s1_total_fratio = (10**(-0.4*s1))/((10**(-0.4*s1))+(10**(-0.4*s2))+(10**(-0.4*s3)))
+	s2_total_fratio = (10**(-0.4*s2))/((10**(-0.4*s1))+(10**(-0.4*s2))+(10**(-0.4*s3)))
+	s3_total_fratio = (10**(-0.4*s3))/((10**(-0.4*s1))+(10**(-0.4*s2))+(10**(-0.4*s3)))
+	print("-----------------------------------")
+	print("Star 1/(Star 1+2+3) ratio:", s1_total_fratio)
+	print("Star 2/(Star 1+2+3) ratio:", s2_total_fratio)
+	print("Star 3/(Star 1+2+3) ratio:", s3_total_fratio)
 
 elif starNum == 2:
 	s1 = float(input("Star 1 Mag : "))
 	s2 = float(input("Star 2 Mag : "))
 
-	s1_fratio = (10**(-0.4*s1))/((10**(-0.4*s1))+(10**(-0.4*s2)))
-	s2_fratio = (10**(-0.4*s2))/((10**(-0.4*s1))+(10**(-0.4*s2)))
+	s1_s2_fratio = (10**(-0.4*s1))/(((10**(-0.4*s2))))
+	s2_s1_fratio = (10**(-0.4*s2))/(((10**(-0.4*s1))))
+	print("-----------------------------------")
+	print("Star 1/Star 2 ratio:", s1_s2_fratio)
+	print("Star 2/Star 1 ratio:", s2_s1_fratio)
 
-	print("Star 1 ratio:", s1_fratio)
-	print("Star 2 ratio:", s2_fratio)
+	s1_total_fratio = (10**(-0.4*s1))/((10**(-0.4*s1))+(10**(-0.4*s2)))
+	s2_total_fratio = (10**(-0.4*s2))/((10**(-0.4*s1))+(10**(-0.4*s2)))
+	print("-----------------------------------")
+	print("Star 1/(Star 1+2) ratio:", s1_total_fratio)
+	print("Star 2/(Star 1+2) ratio:", s2_total_fratio)
 
 else:
 	print("Please type 2 or 3")
