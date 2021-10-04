71fe2b2d14ff42aa624500e2b7b44c73a52e5997
skterry <sterry4261@gmail.com>
2021-10-04 02:00:24 -0700

    avg_err updates

--- analysis/mcmc_chains.py
+++ analysis/mcmc_chains.py
@@ -1,7 +1,6 @@
 ##################
 # Calculate average and standard deviations
-# for parameters from mcmc_fit.dat output.
-# Plot and save each chain.
+# for parameters from *.mcmc outputs.
 ##################
 import os
 import sys
@@ -10,6 +9,12 @@ import pdb
 import numpy as np
 import matplotlib.pyplot as plt
 
+if not os.path.isdir('output/'):
+    os.makedirs('output/')
+    print("created folder : ", 'output/')
+
+else:
+    print('output', "folder already exists.")
 
 def twostarchains(dir_mcmc, dir_out):
 	print("\nStart Analysis")
@@ -52,51 +57,72 @@ def threestarchains(dir_mcmc, dir_out):
 
 
 def avg_err_2star(dir_mcmc, dir_out, N_jackknife, prefix, N_burn):
-	print('----Calculating averages and errors')
-	data = []
-	for ee in range(N_jackknife):
-		lis = np.genfromtxt(dir_mcmc + prefix + str(ee+1) + '_ks.mcmc')
-		data.append(lis)
-	params = ['X1', 'Y1', 'X2', 'Y2', 'Separation[mas]', 'FluxRatio',
-	'FTOTAL', 'F1', 'F2', 'CHISQ']
+	print('----Calculating averages and errors----')
+	finalList = []
 	avgs = []
 	errs = []
 	xsep_avg = []
 	ysep_avg = []
 	xsep_err = []
 	ysep_err = []
-	#print(len(data[0][0]))
-	for i in range(len(data[0][0])):
-		avgs.append(np.average(data[0][N_burn:len(data[0]),i]))
-		errs.append(np.std(data[0][N_burn:len(data[0]),i]))
-	avgs_errs = np.stack((params, avgs, errs), axis=1)
+	finalList = []
+	avgs_errs = []
+	params = ['X1', 'Y1', 'X2', 'Y2', 'Separation[mas]', 'FluxRatio',
+		'FTOTAL', 'F1', 'F2', 'CHISQ', 'X2-X1[mas]', 'Y2-Y1[mas]']
+	for ee in range(N_jackknife):
+		data = []
+		lis = np.genfromtxt(dir_mcmc + prefix + str(ee+1) + '_ks.mcmc')
+		data.append(lis)
+		for i in range(len(data[0][0])):
+			avgs.append(np.average(data[0][N_burn:len(data[0]),i]))
+			errs.append(np.std(data[0][N_burn:len(data[0]),i]))
+	avgs = np.asarray(avgs)
+	avgs2 = avgs.reshape(N_jackknife,10)
+	errs = np.asarray(errs)
+	errs2 = errs.reshape(N_jackknife,10)
+	np.savetxt('test.dat', avgs2, fmt='%s')
+	#mean_rms = np.sqrt(np.mean(errs2[:,5]**2))
+	#sum_rms = np.sqrt(np.sum(errs2[:,5]**2))
+	#jknife_err = np.sqrt((N_jackknife-1)*np.mean(errs2[:,5]**2))
+	#print(mean_rms)
+	#print(sum_rms)
+	#print(jknife_err)
+	final_avg = []
+	final_err = []
+	avgcols = avgs2.shape[1]
+	errcols = errs2.shape[1]
+	for i in range(avgcols):
+		final_avg.append(np.average(avgs2[:,i]))
+	for j in range(errcols):
+		#print(j)
+		final_err.append(np.average(errs2[:,i]))
+	for k in range(N_jackknife): #Calculate separation in mas [9.942 mas/pix for Keck] and append to data.
+		xsep_avg.append(avgs2[k,2]-avgs2[k,0])
+		ysep_avg.append(avgs2[k,3]-avgs2[k,1])
+		xsep_err.append(errs2[k,2]-errs2[k,0])
+		ysep_err.append(errs2[k,3]-errs2[k,1])
+	print(xsep_avg)
 	#--------------------------------------------------------
-	for i in range(len(data[0])):
-		xsep_avg.append((data[0][i,2]-data[0][i,0]))
-		ysep_avg.append((data[0][i,3]-data[0][i,1]))
-		xsep_err.append((data[0][i,2]-data[0][i,0]))
-		ysep_err.append((data[0][i,3]-data[0][i,1]))
-	xsep_avg = np.average(np.asarray(xsep_avg[N_burn:len(xsep_avg)]))*9.942
-	ysep_avg = np.average(np.asarray(ysep_avg[N_burn:len(xsep_err)]))*9.942
-	xsep_err = np.std(np.asarray(xsep_err[N_burn:len(xsep_err)]))*9.942
-	ysep_err = np.std(np.asarray(ysep_err[N_burn:len(ysep_err)]))*9.942
-	xyseps = np.vstack((xsep_avg, ysep_avg))
-	xyseperrs = np.vstack((xsep_err, ysep_err))
-	params2 = ('X2-X1[mas]', 'Y2-Y1[mas]')
-	params2 = np.asarray(params2).reshape(2,1)
-	#params3 = params2.reshape(2,1)
-	xyseps = list(xyseps)
-	xyseperrs = list(xyseperrs)
-	final_seps = np.stack((params2, xyseps, xyseperrs), axis=1)
-	#np.savetxt('./output/seps.dat', finallist)
-	#print(avgs_errs)
-	#print(avgs_errs.shape)
-	new_arr = final_seps.reshape(2,3*1)
-	#print(final_seps)
-	#print(new_arr.shape)
-	#np.savetxt('test.out', final_seps)
-	final_list = np.append(avgs_errs, new_arr, axis=0)
-	np.savetxt(dir_out + 'avg_err_' + str(N_burn) + 'burn.dat', final_list, fmt='%s')
+	#for i in range(len(data[0])): #Calculate separation in mas [9.942 mas/pix for Keck] and append to data.
+	#	xsep_avg.append((data[0][i,2]-data[0][i,0]))
+	#	ysep_avg.append((data[0][i,3]-data[0][i,1]))
+	#	xsep_err.append((data[0][i,2]-data[0][i,0]))
+	#	ysep_err.append((data[0][i,3]-data[0][i,1]))
+	#xsep_avg = np.average(np.asarray(xsep_avg[N_burn:len(xsep_avg)]))*9.942
+	#ysep_avg = np.average(np.asarray(ysep_avg[N_burn:len(xsep_err)]))*9.942
+	#xsep_err = np.std(np.asarray(xsep_err[N_burn:len(xsep_err)]))*9.942
+	#ysep_err = np.std(np.asarray(ysep_err[N_burn:len(ysep_err)]))*9.942
+	#xyseps = np.vstack((xsep_avg, ysep_avg))
+	#xyseperrs = np.vstack((xsep_err, ysep_err))
+	#params2 = ('X2-X1[mas]', 'Y2-Y1[mas]')
+	#params2 = np.asarray(params2).reshape(2,1)
+	#xyseps = list(xyseps)
+	#xyseperrs = list(xyseperrs)
+	#final_seps = np.stack((params2, xyseps, xyseperrs), axis=1)
+	#new_arr = final_seps.reshape(2,3*1)
+	#print(new_arr)
+	#final_list = np.append(avgs_errs, new_arr, axis=0)
+	#np.savetxt(dir_out + 'avg_err_' + str(N_burn) + 'burn.dat', final_list, fmt='%s')
 
 def avg_err_3star(dir_mcmc, dir_out, N_burn):
 	print('----Calculating averages and errors')
