03caa0350d4d003a3c1c346baa9d2891833ee006
skterry <sterry4261@gmail.com>
2021-10-06 15:12:24 -0700

    remove file

--- mcmcjackknife_daophot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in {1..2} # STEP 1
-do
-head -16 input_daophot.txt >> input_daophot.txt
-sed -i -e "$((16*i)),$((16*(i+1)))s/image_1/image_$((i+1))/g" input_daophot.txt
-done
-
-#daophot-mcmc < input_daophot.txt  # STEP 2
